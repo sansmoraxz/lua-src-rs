0994edb2c99a0db61e5b5001d72bd518d35593fc
Souyama Debnath <souyamadebnath@gmail.com>
2026-03-15 23:00:56 +0530

    detailed logs

--- testcrate/src/bin/qemu.rs
+++ testcrate/src/bin/qemu.rs
@@ -70,63 +70,83 @@ fn main() {
     }
 }
 
+fn log_case_start(case: &str) {
+    println!("LUA_SRC_QEMU_CASE_START lua={} case={case}", selected_lua());
+}
+
+fn log_case_ok(case: &str) {
+    println!("LUA_SRC_QEMU_CASE_OK lua={} case={case}", selected_lua());
+}
+
 fn run() -> Result<(), &'static str> {
     unsafe {
+        log_case_start("lua_state_init");
         let state = luaL_newstate();
         if state.is_null() {
-            return Err("luaL_newstate returned null");
+            return Err("lua_state_init: luaL_newstate returned null");
         }
+        log_case_ok("lua_state_init");
 
+        log_case_start("open_libs");
         luaL_openlibs(state);
+        log_case_ok("open_libs");
 
+        log_case_start("version_string");
         lua_getglobal(state, c"_VERSION".as_ptr());
         if to_string(state, -1) != expected_version() {
-            return Err("unexpected Lua version");
+            return Err("version_string: unexpected Lua version");
         }
+        log_case_ok("version_string");
 
+        log_case_start("return_ok_chunk");
         if luaL_loadstring(state, c"return 'ok'".as_ptr()) != 0 {
-            return Err("luaL_loadstring failed");
+            return Err("return_ok_chunk: luaL_loadstring failed");
         }
         if lua_pcall(state, 0, 1, 0) != 0 {
-            return Err("lua_pcall failed");
+            return Err("return_ok_chunk: lua_pcall failed");
         }
         if to_string(state, -1) != "ok" {
-            return Err("unexpected Lua result");
+            return Err("return_ok_chunk: unexpected Lua result");
         }
+        log_case_ok("return_ok_chunk");
 
         unsafe extern "C-unwind" fn it_panics(state: *mut c_void) -> c_int {
             unsafe { luaL_error(state, c"exception!".as_ptr()) }
         }
 
+        log_case_start("panic_bridge");
         lua_pushcclosure(state, it_panics, 0);
         if lua_pcall(state, 0, 0, 0) != 2 {
-            return Err("lua_pcall did not return LUA_ERRRUN");
+            return Err("panic_bridge: lua_pcall did not return LUA_ERRRUN");
         }
         if to_string(state, -1) != "exception!" {
-            return Err("unexpected exception text");
+            return Err("panic_bridge: unexpected exception text");
         }
+        log_case_ok("panic_bridge");
 
+        log_case_start("unicode_identifier");
         let ret = luaL_loadstring(state, UCID_CHUNK.as_ptr().cast());
         #[cfg(feature = "lua54")]
         {
             if ret != 0 {
-                return Err("unicode identifier chunk did not compile");
+                return Err("unicode_identifier: chunk did not compile");
             }
             if lua_pcall(state, 0, 0, 0) != 0 {
-                return Err("unicode identifier chunk did not run");
+                return Err("unicode_identifier: chunk did not run");
             }
             lua_getglobal(state, UCID_NAME.as_ptr().cast());
             if to_string(state, -1) != UCID_VALUE {
-                return Err("unexpected unicode identifier value");
+                return Err("unicode_identifier: unexpected value");
             }
         }
 
         #[cfg(not(feature = "lua54"))]
         {
             if ret == 0 {
-                return Err("unicode identifier chunk unexpectedly compiled");
+                return Err("unicode_identifier: chunk unexpectedly compiled");
             }
         }
+        log_case_ok("unicode_identifier");
     }
 
     Ok(())
